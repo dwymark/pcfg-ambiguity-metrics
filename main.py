21b36767e955c22117cd9b145c4ef606e965c7ea
Daniel Wymark <dwymark@ucla.edu>
2015-11-08 15:35:30 -0800

    Binarized the PCFG
    Applied CNF normalization to trees before inducing PCFG so as to
    binarize it's productions.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+#!python3
 import nltk.grammar as grammar
 from nltk.corpus import treebank
+from nltk.treetransforms import chomsky_normal_form
 
 def trained_pcfg():
   productions = []
   for t in treebank.parsed_sents():
+    tr = t
+    chomsky_normal_form(tr)
     for p in t.productions():
       productions.append(p)
   return grammar.induce_pcfg(grammar.Nonterminal('S'), productions)
 
 if __name__ == '__main__':
   tb_pcfg = trained_pcfg()
+  print(tb_pcfg.productions(lhs=grammar.Nonterminal('S')))
