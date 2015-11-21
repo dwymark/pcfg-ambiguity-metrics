56e729893ce33b632cc8bc5cd7cfaa573674d9d3
Daniel Wymark <dwymark@ucla.edu>
2015-11-20 18:15:11 -0800

    Cache PCFG in Text File
    Edited trained_pcfg() to store a local cache of the treebank productions

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-#!python3
+﻿#!python3
 import nltk.grammar as grammar
 from nltk.corpus import treebank
 from nltk.treetransforms import chomsky_normal_form
 
 def trained_pcfg():
-  productions = []
-  for t in treebank.parsed_sents():
-    tr = t
-    chomsky_normal_form(tr)
-    for p in t.productions():
-      productions.append(p)
-  return grammar.induce_pcfg(grammar.Nonterminal('S'), productions)
+  try:
+    f = open("pcfgcache.txt",'r')
+    gram = PCFG.from_string(f.read())
+    f.close()
+    return gram
+  except FileNotFoundError:
+    f = open("pcfgcache.txt",'w')
+    print("Training the PCFG...")
+    productions = []
+    for t in treebank.parsed_sents():
+      tr = t
+      chomsky_normal_form(tr)
+      for p in t.productions():
+        productions.append(p)
+        f.write("{0}\n".format(p.__str__()))
+    print("Trained!")
+    f.close()
+    return grammar.induce_pcfg(grammar.Nonterminal('S'), productions)
 
 if __name__ == '__main__':
   tb_pcfg = trained_pcfg()
