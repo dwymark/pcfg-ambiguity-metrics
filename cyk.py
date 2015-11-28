730c9544b3d836426f27b7dc2f11b22704329c31
Daniel Wymark <dwymark@ucla.edu>
2015-11-27 16:06:27 -0800

    Trying to Setup Cache

--- cyk.py
+++ cyk.py
@@ -65,8 +65,10 @@ def test1(test_str):
 
 def test2():
   pcfg = main.trained_pcfg()
-  CYK(pcfg,"He crossed the road".split(" "))[0].draw()
+  try:
+    CYK(pcfg,"He crossed the road".split(" "))[0].draw()
+  try:
+    CYK(pcfg,"The man saw the boy with the telescope .".split(" "))[0].draw()
 
 if __name__ == '__main__':
-  test1("a a a a a a a a a a")
-  CYK(toy_pcfg,"a b a".split(" "))[0].draw()
+  test2()
--- main.py
+++ main.py
@@ -4,25 +4,28 @@ from nltk.corpus import treebank
 from nltk.treetransforms import chomsky_normal_form, collapse_unary
 
 def trained_pcfg():
-#  try:
-#    f = open("pcfgcache.txt",'r')
-#    gram = PCFG.from_string(f.read())
-#    f.close()
-#    return gram
-#  except FileNotFoundError:
-    #f = open("pcfgcache.txt",'w')
+   try:
+     f = open("doesntexist.txt",'r')
+     print("Loading the PCFG...")
+     gram = grammar.PCFG.fromstring(f.read())
+     f.close()
+     print("Loaded!")
+     return gram
+   except FileNotFoundError:
+    f = open("pcfgcache.txt",'w')
     print("Training the PCFG...")
     productions = []
     for t in treebank.parsed_sents():
       collapse_unary(t,True)
       chomsky_normal_form(t)
-      print(t)
-      t.draw()
+      #t.draw()
       for p in t.productions():
         productions.append(p)
     gram = grammar.induce_pcfg(grammar.Nonterminal('S'), productions)
+    for production in gram.productions():
+      f.write(production.__str__() + '\n')
+    f.close()
     print("Trained!")
-    #f.close()
     return gram
 
 if __name__ == '__main__':
